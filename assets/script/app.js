66579f815420bbea1e915c487cd81313ab21ffc5
mahmudul1995 <mahmuduls451@gmail.com>
2022-03-24 07:21:49 +0600

    complete

--- assets/script/app.js
+++ assets/script/app.js
@@ -8,3 +8,17 @@ function clr() {
 function del() {
     document.getElementById('outputScreen').innerText = document.getElementById('outputScreen').value.slice(0, -1);
 }
+// Making button works 
+function display(n) {
+    document.getElementById('outputScreen').innerText += n;
+}
+
+// making the calculaton 
+function calc(){
+    try{
+        document.getElementById('outputScreen').innerText = eval(document.getElementById('outputScreen').innerText);
+    }
+    catch{
+        document.getElementById('outputScreen').innerText = 'আমি ক্যালকুলটর হইলেও মানুষ ভাই!';
+    }
+}
